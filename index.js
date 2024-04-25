b12edb5649ceb850f06d2e62f9fe5dcb27bdadd1
Julien Miguel <miguel.julien.pro@gmail.com>
2024-04-25 17:31:12 +0200

    Part 1 - done
    Signed-off-by: Julien Miguel <miguel.julien.pro@gmail.com>

--- index.js
+++ index.js
@@ -3,4 +3,28 @@ import { readFileSync } from "fs";
 const test = readFileSync("test.txt", "utf8");
 const final = readFileSync("final.txt", "utf8");
 
-console.log({ test, final });
+const part1 = (data) => {
+  const fishes = final.split(",").map(Number);
+
+  let day = 0;
+  while (day < 80) {
+    const fishesLength = fishes.length;
+    for (let i = 0; i < fishesLength; i++) {
+      if (fishes[i] === 0) {
+        fishes.push(8);
+        fishes[i] = 6;
+      } else {
+        fishes[i]--;
+      }
+    }
+
+    day++;
+  }
+
+  return {
+    fishes,
+    length: fishes.length,
+  };
+};
+
+console.log({ part1: part1(final) });
